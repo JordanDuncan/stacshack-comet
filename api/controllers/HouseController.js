8999ce4f8cb0ad06b37d694d8dfef9c756cc8294
Jordan Duncan <jordan@dogfi.sh>
2015-02-21 13:56:53 +0000

    new controller set

---
+++ api/controllers/HouseController.js
@@ -0,0 +1,11 @@
+/**
+ * HouseController
+ *
+ * @description :: Server-side logic for managing houses
+ * @help        :: See http://links.sailsjs.org/docs/controllers
+ */
+
+module.exports = {
+	
+};
+
